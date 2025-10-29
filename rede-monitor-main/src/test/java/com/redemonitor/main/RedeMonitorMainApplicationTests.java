811367c78b3b84e5a1ac57ee07f2e1335dbc2b84
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-10-29 15:44:19 -0300

    feat: Após Migrado Para o Spring Tool Suite 4

---
+++ rede-monitor-main/src/test/java/com/redemonitor/main/RedeMonitorMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redemonitor.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeMonitorMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
