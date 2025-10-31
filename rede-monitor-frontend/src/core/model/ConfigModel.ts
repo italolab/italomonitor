4cb984916bdab8ee26fc63f25ec5f2daa4465090
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-10-31 16:51:00 -0300

    feat: Fazendo Funcionar o Consumo dos Endpoints via REST Pelos Microserviços

--- rede-monitor-frontend/src/core/model/ConfigModel.ts
+++ rede-monitor-frontend/src/core/model/ConfigModel.ts
@@ -12,7 +12,7 @@ export class ConfigModel {
     }
 
     async getConfig() {
-        return await api.get( "/config" );
+        return await api.get( "/config/get" );
     }
 
 }
--- run.bat
+++ run.bat
@@ -1,13 +1,22 @@
 @echo off
 
-if %1 == upbase (
-    docker compose up --build rabbitmq rede-monitor-db -d
+if %1 == upall (
+    call :upbase
+    call :upmain
+    call :updm
+    call :upfront
 ) else if %1 == upapp (
-    cd rede-monitor-main/
-    .\mvnw clean package -DskipTests
-    cd ..
-
-    docker compose up --build rede-monitor-main rede-monitor-frontend -d
+    call :upmain
+    call :updm
+    call :upfront
+) else if %1 == upbase (
+    call :upbase
+) else if %1 == upmain (
+    call :upmain
+) else if %1 == updm (
+    call :updm
+) else if %1 == upfront (
+    call :upfront
 ) else if %1 == up (
     docker compose %*
 ) else if %1 == down (
@@ -15,3 +24,32 @@ if %1 == upbase (
 ) else (
     echo Comando invalido.
 )
+
+goto :fim
+
+:upmain
+    cd rede-monitor-main/
+    call .\mvnw.cmd clean package -DskipTests
+    cd ..
+
+    docker compose up --build rede-monitor-main -d
+exit /b 0
+
+:updm
+    cd rede-monitor-disp-monitor/
+    call .\mvnw.cmd clean package -DskipTests
+    cd ..
+
+    docker compose up --build rede-monitor-disp-monitor -d
+exit /b 0
+
+:upfront
+    docker compose up --build rede-monitor-frontend -d
+exit /b 0
+
+:upbase
+    docker compose up --build rabbitmq rede-monitor-db -d
+exit /b 0
+
+
+:fim
