1ef07f458506a06fac2fc6e4e9c0e45f3c529250
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-11-12 15:11:17 -0300

    fix: Parece Consertado o Problema com Log Excecivo de Desconexão de WebSockets

--- main-api/src/main/java/com/redemonitor/main/config/WebSocketConfig.java
+++ main-api/src/main/java/com/redemonitor/main/config/WebSocketConfig.java
@@ -1,14 +1,21 @@
 package com.redemonitor.main.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.redemonitor.main.security.WebSocketAuthInterceptor;
+
+/*
+ * O tempo de expiração do refresh token define o tempo de vida da sessão do websocket
+ */
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,9 +24,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     private WebSocketAuthInterceptor webSocketAuthInterceptor;
     
+    @Value("${jwt.refresh_token.expire.at}")
+    private String refreshTokenExpireAt;
+    
+    @Bean
+    ServletServerContainerFactoryBean createWebSocketContainer() {
+    	ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+    	container.setMaxSessionIdleTimeout( Integer.parseInt( refreshTokenExpireAt ) * 1000L ); 
+    	return container;
+    }
+    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker( "/topic" );
+        registry.enableSimpleBroker( "/topic" ); 
         registry.setUserDestinationPrefix( "/user" );
         registry.setApplicationDestinationPrefixes( "/app" );
     }
