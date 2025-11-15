35f5ba8a43ee6389ae31bf15c9a43ecb256a529f
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-11-15 15:33:34 -0300

    style: Alterando o Estilo CSS

--- main-api/src/main/java/com/italomonitor/main/ItaloMonitorMainApplication.java
+++ main-api/src/main/java/com/italomonitor/main/ItaloMonitorMainApplication.java
@@ -2,7 +2,6 @@ package com.italomonitor.main;
 
 import java.util.Random;
 
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
